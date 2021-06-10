515e76e58997e9993400b0877a988c2fa11caf4e
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-10 00:04:30 -0300

    Save blog content and comments to a JSON file

--- src/server.js
+++ src/server.js
@@ -1,61 +1,22 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 
+let blogiumData = {
+    posts: [],
+    comments: [],
+    idCounter: { post: 0, comment: 0 },
+};
+if (fs.existsSync("./data/blogiumData.json")) {
+    blogiumData = JSON.parse(fs.readFileSync("./data/blogiumData.json"));
+}
+
+function saveJSON() {
+    fs.writeFileSync("./data/blogiumData.json", JSON.stringify(blogiumData));
+}
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-const blogiumData = {
-    posts: [
-        {
-            id: 1,
-            title: "Hello World",
-            coverUrl:
-                "https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png",
-            contentPreview:
-                "Esta é a estrutura de um post esperado pelo front-end",
-            content:
-                "Este é o conteúdo do post, o que realmente vai aparecer na página do post...",
-            commentCount: 2,
-        },
-        {
-            id: 2,
-            title: "Segundo Post",
-            coverUrl:
-                "https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png",
-            contentPreview: "Esta é o segundo post",
-            content: "Este é o conteúdo do SEGUNDO post, O MELHOR POST...",
-            commentCount: 2,
-        },
-    ],
-    comments: [
-        {
-            id: 1,
-            postId: 1,
-            author: "João",
-            content: "Muito bom esse post! Tá de parabéns",
-        },
-        {
-            id: 2,
-            postId: 1,
-            author: "Maria",
-            content: "Como faz pra dar palmas?",
-        },
-        {
-            id: 3,
-            postId: 2,
-            author: "Anakin",
-            content: "Esse post é brabo",
-        },
-        {
-            id: 4,
-            postId: 2,
-            author: "Darth Vader",
-            content: "Venha para o lado sombrio!",
-        },
-    ],
-    idCounter: { post: 2, comment: 4 },
-};
 
 const posts = blogiumData.posts;
 const comments = blogiumData.comments;
@@ -77,6 +38,7 @@ app.post("/posts", (req, res) => {
         commentCount: 0,
     });
     idCounter.post++;
+    saveJSON();
     res.send("OK");
 });
 
@@ -92,6 +54,7 @@ app.post("/posts/:id/comments", (req, res) => {
     const post = posts.find((p) => p.id === parseInt(req.params.id));
     post.commentCount++;
     idCounter.comment++;
+    saveJSON();
     res.send("OK");
 });
 
@@ -100,12 +63,14 @@ app.put("/posts/:id", (req, res) => {
     post.title = req.body.title;
     post.coverUrl = req.body.coverUrl;
     post.content = req.body.content;
+    saveJSON();
     res.send("OK");
 });
 
 app.delete("/posts/:id", (req, res) => {
     const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
     posts.splice(index, 1);
+    saveJSON();
     res.send("OK");
 });
 
